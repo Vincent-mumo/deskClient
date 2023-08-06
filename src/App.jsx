3662c2e65dca593c4776edf5d691f16770986192
vincent <vincent@kali.kali>
2023-08-06 23:53:54 +0300

    configured home

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import "./App.css"
+import { AuthContext } from "./context/AuthContext"
 
 function App() {
+  const {user} = useContext(AuthContext)
 
   return (
    <div className="app">
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={user?<Home/> : <Login/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
