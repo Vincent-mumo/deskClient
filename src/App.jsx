58cbcfbb80a86a3cc466aae2aa7be68195f6fbdd
vincent <vincent@kali.kali>
2023-08-07 00:04:44 +0300

    added use Context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import "./App.css"
 import { AuthContext } from "./context/AuthContext"
+import { useContext } from "react"
 
 function App() {
   const {user} = useContext(AuthContext)
