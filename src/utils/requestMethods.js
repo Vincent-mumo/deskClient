781783200c4836d7527901f6d2a97e850a9c73bf
vincent <vincent@kali.kali>
2023-08-08 11:56:36 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://deskapi.onrender.com/api/"
+const BASE_URL = "https://deskbackend.onrender.com/api/"
 
 export const apiRequests = axios.create({
     baseURL:BASE_URL,withCredentials:true
