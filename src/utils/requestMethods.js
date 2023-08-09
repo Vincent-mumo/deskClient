1f76f709b44c75ba0786be2fa2c9efeb26f650cd
vincent <vincent@kali.kali>
2023-08-09 21:45:39 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://deskbackend.onrender.com/api/"
+const BASE_URL = "https://desk-api.vercel.app/api/"
 
 export const apiRequests = axios.create({
     baseURL:BASE_URL,withCredentials:true
