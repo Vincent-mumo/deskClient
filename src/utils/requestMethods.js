2e026e709961ec109031b6237aeddd07f3c8f829
vincent <vincent@kali.kali>
2023-08-10 10:16:25 +0300

    added link

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://desk-api.vercel.app/api/"
+const BASE_URL = "https://desk-api-vincent-mumo.vercel.app/api/"
 
 export const apiRequests = axios.create({
     baseURL:BASE_URL,withCredentials:true
